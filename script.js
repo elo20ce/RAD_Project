56e767df3aebe702e534fc14ae145044851d5f89
Mawulolo TONA <tonamawulolo@gmail.com>
2024-01-13 06:43:38 +0000

    modification pas terminer . ça promet

--- script.js
+++ script.js
@@ -10,3 +10,8 @@ document.querySelectorAll('input[type="number"]').forEach(inputNumber => {
         if(inputNumber.value.length > inputNumber.maxLength) inputNumber.value = inputNumber.value.slice(0, inputNumber.maxLength);
     };
 });
+
+AOS.init({
+    duration: 400,
+    delay: 200,
+});
